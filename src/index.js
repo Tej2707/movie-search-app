da0605cd8885645cb610bea839d9af55d0345440
Tej Prakash Sharma <tejsharma2707@gmail.com>
2023-07-11 16:35:25 +0530

    Deployed website

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SearchMovies from './searchMovies';
+
+class Main extends React.Component{
+  render(){
+    return(
+      <div className='container'>
+      <h1 className='title'> React Movie Search</h1>
+      <SearchMovies/>
+      </div>
+    );
+  }
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <Main />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
